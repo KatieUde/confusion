f4a9df4407fd58e13123010e72ca12018a711a70
Katie Ude <katie.ude@gmail.com>
2019-05-26 16:27:15 -0500

    completed assignment 1 -- added a new DishDetail component, included the DishDetail in the MenuComponent's view to show the selected dish, passing the selectedDish as props to the DishDetail component, used appropriate bootstrap classes for styling the elements, used the Card component to display the details of teh dish in the DishDetail component, included a list of comments about the dish into the DishDetail

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle
+} from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 
 class Menu extends Component {
     constructor(props) {
@@ -14,24 +20,6 @@ class Menu extends Component {
       this.setState({
         selectedDish: dish
       });
-    }
-
-    renderDish(dish) {
-      if (dish != null) {
-        return (
-          <Card>
-            <CardImg top src={dish.image} alt={dish.name} />
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
-        );
-      } else {
-        return (
-          <div></div>
-        );
-      }
     }
 
     render() {
@@ -56,9 +44,7 @@ class Menu extends Component {
           <div className="row">
             {menu}
           </div>
-          <div className="row">
-            {this.renderDish(selectedDish)}
-          </div>
+          <DishDetail dish={selectedDish}/>
         </div>
       );
     }
