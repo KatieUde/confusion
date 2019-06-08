802debef7418c55cb96e1e18008d3a6f5eba08b9
Katie Ude <katie.ude@gmail.com>
2019-06-08 16:01:15 -0500

    Controlled Form Validation -- working on adding basic form validation to the ContactComponent

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -7,7 +7,9 @@ import {
   FormGroup,
   Label,
   Input,
-  Col
+  Col,
+  Row,
+  FormFeedback
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
@@ -22,11 +24,18 @@ class Contact extends Component {
       email: '',
       agree: false,
       contactType: 'Tel.',
-      message: ''
+      message: '',
+      touched: {
+        firstname: false,
+        lastname: false,
+        telnum: false,
+        email: false
+      }
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
   }
 
   handleInputChange(event) {
@@ -45,7 +54,48 @@ class Contact extends Component {
     event.preventDefault();
   }
 
+  handleBlur = (field) => (event) => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true}
+    });
+  }
+
+  validate(firstname, lastname, telnum, email) {
+    const errors = {
+      firstname: '',
+      lastname: '',
+      telnum: '',
+      email: '',
+    };
+    const reg = /^\d+$/;
+
+    if (this.state.touched.firstname && firstname.length < 3) {
+      errors.firstname = 'First Name should be >= 3 characters';
+    } else if (this.state.touched.firstname && firstname.length > 10) {
+      errors.firstname = 'First Name should be <= 10 characters';
+    }
+
+    if (this.state.touched.lastname && lastname.length < 3) {
+      errors.lastname = 'Last Name should be >= 3 characters';
+    } else if (this.state.touched.lastname && lastname.length > 10) {
+      errors.lastname = 'Last Name should be <= 10 characters';
+    }
+
+    if (this.state.touched.telnum && !reg.test(telnum)) {
+      errors.telnum = 'Telephone Number should contain only numbers';
+    }
+
+    if (this.state.touched.email && email.split('').filter(x => x === '@').length !== 1) {
+      errors.email = 'Email address should contain a @';
+    }
+
+    return errors;
+  }
+
   render() {
+    const { firstname, lastname, telnum, email, agree, contactType, message, touched } = this.state;
+    const errors = this.validate(firstname, lastname, telnum, email);
+
     return (
       <div className="container">
         <div className="row">
@@ -106,9 +156,13 @@ class Contact extends Component {
                     id="firstname"
                     name="firstname"
                     placeholder="First Name"
-                    value={this.state.firstname}
+                    value={firstname}
+                    valid={errors.firstname === ''}
+                    invalid={errors.firstname !== ''}
+                    onBlur={this.handleBlur('firstname')}
                     onChange={this.handleInputChange}
                   />
+                  <FormFeedback>{errors.firstname}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -119,9 +173,13 @@ class Contact extends Component {
                     id="lastname"
                     name="lastname"
                     placeholder="Last Name"
-                    value={this.state.lastname}
+                    value={lastname}
+                    valid={errors.lastname === ''}
+                    invalid={errors.lastname !== ''}
+                    onBlur={this.handleBlur('lastname')}
                     onChange={this.handleInputChange}
                   />
+                  <FormFeedback>{errors.lastname}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -132,9 +190,13 @@ class Contact extends Component {
                     id="telnum"
                     name="telnum"
                     placeholder="Tel. number"
-                    value={this.state.telnum}
+                    value={telnum}
+                    valid={errors.telnum === ''}
+                    invalid={errors.telnum !== ''}
+                    onBlur={this.handleBlur('telnum')}
                     onChange={this.handleInputChange}
                   />
+                  <FormFeedback>{errors.telnum}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -145,9 +207,13 @@ class Contact extends Component {
                     id="email"
                     name="email"
                     placeholder="Email"
-                    value={this.state.email}
+                    value={email}
+                    valid={errors.email === ''}
+                    invalid={errors.email !== ''}
+                    onBlur={this.handleBlur('email')}
                     onChange={this.handleInputChange}
                   />
+                  <FormFeedback>{errors.email}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -157,7 +223,7 @@ class Contact extends Component {
                       <Input
                         type="checkbox"
                         name="agree"
-                        checked={this.state.agree}
+                        checked={agree}
                         onChange={this.handleInputChange}
                       />{' '}
                       <strong>May we contact you?</strong>
@@ -168,7 +234,7 @@ class Contact extends Component {
                   <Input
                     type="select"
                     name="contactType"
-                    value={this.state.contactType}
+                    value={contactType}
                     onChange={this.handleInputChange}
                   >
                     <option>Tel.</option>
@@ -184,7 +250,7 @@ class Contact extends Component {
                     id="message"
                     name="message"
                     rows="12"
-                    value={this.state.message}
+                    value={message}
                     onChange={this.handleInputChange}
                   />
                 </Col>
